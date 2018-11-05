497c21e85f1f080b45884a05c989e8cef453a55e
Daniel Badea <daniel.badea@windriver.com>
2018-11-05 06:51:40 -0200

    Use user root to run ceph services
    Avoid debugging file permission issues when upgrading to Jewel.
    This is done to provide the same setup as Hammer in StarlingX.
    This commit should be reverted when we decide to enable the ceph user.
    
    Signed-off-by: Daniel Badea <daniel.badea@windriver.com>

--- src/ceph-disk/ceph_disk/main.py
+++ src/ceph-disk/ceph_disk/main.py
@@ -3201,8 +3201,7 @@ def mkfs(
                 '--monmap', monmap,
                 '--osd-data', path,
                 '--osd-uuid', fsid,
-                '--setuser', get_ceph_user(),
-                '--setgroup', get_ceph_group(),
+                 # don't run as ceph
             ],
         )
     elif osd_type == 'filestore':
@@ -3217,8 +3216,7 @@ def mkfs(
                 '--osd-data', path,
                 '--osd-journal', os.path.join(path, 'journal'),
                 '--osd-uuid', fsid,
-                '--setuser', get_ceph_user(),
-                '--setgroup', get_ceph_group(),
+                 # don't run as ceph
             ],
         )
     else:
